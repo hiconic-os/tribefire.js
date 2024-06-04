224a82c3cb732a3131afaaca1340ab0eb0c2361d
Peter Gazdik <peter.gazdik@gmail.com>
2024-06-04 15:42:30 +0200

    Changing tf-js loading log to say hicocnic

--- tribefire-js-module/src/com/braintribe/gwt/tribefirejs/linker/tribefire.js
+++ tribefire-js-module/src/com/braintribe/gwt/tribefirejs/linker/tribefire.js
@@ -1,16 +1,14 @@
 /*!
- * tribefire.js JavaScript Library v{tf-version}
- *
- * Copyright Braintribe IT
+ * hiconic.js JavaScript Library v{tf-version}
  *
  * Date: {tf-date}
  */
-console.time('start-tribefire-js');
+console.time('start-hiconic-js');
 var $wnd = window;
 var $doc = $wnd.document;
 {tf-script}
 $wnd.$tf.version = function(){
 	return "{tf-version}";
 }
-console.timeEnd('start-tribefire-js');
-console.log("tribefire.js JavaScript Library {tf-version}");
+console.timeEnd('start-hiconic-js');
+console.log("hiconic.js JavaScript Library {tf-version}");
