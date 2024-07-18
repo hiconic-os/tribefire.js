95981805146502f21c36be826ff8c580374fd95e
Peter Gazdik <peter.gazdik@gmail.com>
2024-07-18 17:09:06 +0200

    tribefire.js is now an ES6 module

--- tribefire-js-module/src/com/braintribe/gwt/tribefirejs/linker/tribefire.js
+++ tribefire-js-module/src/com/braintribe/gwt/tribefirejs/linker/tribefire.js
@@ -1,12 +1,14 @@
-/*!
- * hiconic.js JavaScript Library v{tf-version}
+/* hiconic.js JavaScript Library v{tf-version}
  *
  * Date: {tf-date}
  */
+export const $tf = globalThis.$tf = {};
+export const $T = globalThis.$T = {};
 console.time('start-hiconic-js');
-var $wnd = window;
-var $doc = $wnd.document;
+var $wnd = globalThis;
+// generated script start
 {tf-script}
+// generated script end
 $wnd.$tf.version = function(){
 	return "{tf-version}";
 }
